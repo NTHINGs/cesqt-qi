4f11b0c1d739a877ec1302b41127ee4f09751a7e
Mauricio Martinez <n_othing@hotmail.com>
2018-10-12 12:18:19 -0500

    Construyendo Pagina de administrador

--- admin-templates/admin-page.php
+++ admin-templates/admin-page.php
@@ -4,69 +4,19 @@
  * cesqt Tabbed Settings Page
  */
 
-add_action( 'init', 'cesqt_admin_init' );
-add_action( 'admin_menu', 'cesqt_settings_page_init' );
+add_action( 'admin_menu', 'cesqt_qi_admin' );
 
-function cesqt_admin_init() {
-	$settings = get_option( "cesqt_theme_settings" );
-	if ( empty( $settings ) ) {
-		$settings = array(
-			'cesqt_intro' => 'Some intro text for the home page',
-			'cesqt_tag_class' => false,
-			'cesqt_ga' => false
-		);
-		add_option( "cesqt_theme_settings", $settings, '', 'yes' );
-	}	
+function cesqt_qi_admin() {
+    $hook = add_menu_page(
+        'Cuestionario Calidad de Vida Laboral',     // page title
+        'Cuestionario Calidad de Vida Laboral',     // menu title
+        'cesqt',   // capability
+        'cuestionario-cesqt',     // menu slug
+		'render_cesqt_qi_admin', // callback function
+		'dashicons-universal-access'
+    );
 }
 
-function cesqt_settings_page_init() {
-	$theme_data = get_theme_data( TEMPLATEPATH . '/style.css' );
-	$settings_page = add_theme_page( $theme_data['Name']. ' Theme Settings', $theme_data['Name']. ' Theme Settings', 'edit_theme_options', 'theme-settings', 'cesqt_settings_page' );
-	add_action( "load-{$settings_page}", 'cesqt_load_settings_page' );
-}
-
-function cesqt_load_settings_page() {
-	if ( $_POST["cesqt-settings-submit"] == 'Y' ) {
-		check_admin_referer( "cesqt-settings-page" );
-		cesqt_save_theme_settings();
-		$url_parameters = isset($_GET['tab'])? 'updated=true&tab='.$_GET['tab'] : 'updated=true';
-		wp_redirect(admin_url('themes.php?page=theme-settings&'.$url_parameters));
-		exit;
-	}
-}
-
-function cesqt_save_theme_settings() {
-	global $pagenow;
-	$settings = get_option( "cesqt_theme_settings" );
-	
-	if ( $pagenow == 'themes.php' && $_GET['page'] == 'theme-settings' ){ 
-		if ( isset ( $_GET['tab'] ) )
-	        $tab = $_GET['tab']; 
-	    else
-	        $tab = 'homepage'; 
-
-	    switch ( $tab ){ 
-	        case 'general' :
-				$settings['cesqt_tag_class']	  = $_POST['cesqt_tag_class'];
-			break; 
-	        case 'footer' : 
-				$settings['cesqt_ga']  = $_POST['cesqt_ga'];
-			break;
-			case 'homepage' : 
-				$settings['cesqt_intro']	  = $_POST['cesqt_intro'];
-			break;
-	    }
-	}
-	
-	if( !current_user_can( 'unfiltered_html' ) ){
-		if ( $settings['cesqt_ga']  )
-			$settings['cesqt_ga'] = stripslashes( esc_textarea( wp_filter_post_kses( $settings['cesqt_ga'] ) ) );
-		if ( $settings['cesqt_intro'] )
-			$settings['cesqt_intro'] = stripslashes( esc_textarea( wp_filter_post_kses( $settings['cesqt_intro'] ) ) );
-	}
-
-	$updated = update_option( "cesqt_theme_settings", $settings );
-}
 
 function cesqt_admin_tabs( $current = 'homepage' ) { 
     $tabs = array( 'homepage' => 'Home', 'general' => 'General', 'footer' => 'Footer' ); 
@@ -81,7 +31,7 @@ function cesqt_admin_tabs( $current = 'homepage' ) {
     echo '</h2>';
 }
 
-function cesqt_settings_page() {
+function render_cesqt_qi_admin() {
 	global $pagenow;
 	$settings = get_option( "cesqt_theme_settings" );
 	$theme_data = get_theme_data( TEMPLATEPATH . '/style.css' );
