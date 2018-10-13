d8fb1f2e50dfd9ffce428db972487f6cf7f07c76
Mauricio Martinez <n_othing@hotmail.com>
2018-10-13 13:17:27 -0500

    Construyendo graficas

--- admin-templates/admin-page.php
+++ admin-templates/admin-page.php
@@ -44,7 +44,7 @@ function render_cesqt_qi_admin() {
 	<div class="wrap">
         <h2><?php echo $title; ?></h2>
         <p>Tu link para compartir el cuestionario de resiliencia a tus empleados es: </p>
-        <a href="/cuestionario-cesqt/?org_id=%HASH%">%SITE_URL%/cuestionario-cesqt/?org_id=%HASH%</a>
+        <a href="/cuestionario-cesqt/?org_id=%HASH%"><?php echo get_site_url();?>/cuestionario-cesqt/?org_id=%HASH%</a>
 		
 		<?php			
             if ( isset ( $_GET['tab'] ) ) cesqt_admin_tabs($_GET['tab']); else cesqt_admin_tabs('INFORMACION');
